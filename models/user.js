70e3fe9f2d9e1cbf131e137b89f4aa6e93ee908c
Timothy Dusterdieck <timothy@masseywholesale.com>
2017-05-13 10:43:29 -0400

    authentication added to user model

--- models/user.js
+++ models/user.js
@@ -1,13 +1,9 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs"); 
+// Creating our User model
 module.exports = function(sequelize, DataTypes) { 
   var User = sequelize.define("User", {
-    username: { 
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-        is: /^[a-z]+$/i
-      }
-    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -64,7 +60,23 @@ module.exports = function(sequelize, DataTypes) {
           });
 
         }
+      },
+      // Creating a custom method for our User model. This will check if an unhashed password entered by
+      // The user can be compared to the hashed password stored in our database
+      instanceMethods: {
+        validPassword: function(password) {
+          return bcrypt.compareSync(password, this.password);
+        }
+      },
+      // Hooks are automatic methods that run during various phases of the User Model lifecycle
+      // In this case, before a User is created, we will automatically hash their password
+      hooks: {
+        beforeCreate: function(user, options, cb) {
+          user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+          cb(null, options);
+        }
       }
+
     }
   );
   return User;
